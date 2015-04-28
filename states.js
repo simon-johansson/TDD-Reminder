2ed4e32cd02e8cd95e0932cf52f107732bcf13c8
Simon Johansson <mail@simon-johansson.com>
2015-04-28 22:51:22 +0200

    Move out state data to seperate file

---
+++ states.js
@@ -0,0 +1,13 @@
+module.exports = [{
+    label: 'Tests have been written and are failing',
+    icon: 'assets/red.png',
+    phase: 'red'
+  },{
+    label: 'Implementation is done, tests are passing',
+    icon: 'assets/green.png',
+    phase: 'green'
+  },{
+    label: 'Refactoring has been made, code is clean',
+    icon: 'assets/blue.png',
+    phase: 'refactor'
+}];
